22f4cbaf6f5a24b278d81e624acc97c313a01632
Troy <tydamon@hotmail.com>
2024-08-25 23:29:43 +1000

    added style css

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import webbrowser
+import threading
 
 app = Flask(__name__)
 
@@ -6,5 +8,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+def open_browser():
+    webbrowser.open('http://127.0.0.1:5000/')
+
 if __name__ == '__main__':
+    threading.Thread(target=open_browser).start()
     app.run(debug=True)
